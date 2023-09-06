93aca92d6bfd2ebb650d7be02d58cd7f1c322dac
Harry <77893070+harrisonMondragon@users.noreply.github.com>
2023-09-06 13:49:49 -0600

    Add output folder creation
    Add a function to create an output folder. The second arguement is the
    folder.

--- invoices_suck.py
+++ invoices_suck.py
@@ -1,16 +1,23 @@
 import sys
 import pandas as pd
-from pathlib import Path
+from pathlib import Path, PurePath
 
 def main():
 
-    sub_folder = Path(sys.argv[1])
-    sub_files = sub_folder.glob('*')
+    sub_files = Path(sys.argv[1]).glob('*')
+    create_output_folder(sys.argv[2])
 
     for file in sub_files:
         sub_df = pd.read_excel(file)
-        print(sub_df)
+        sub_df.sort_values(by='BILL_DATE')
 
+        # print(sub_df)
+
+def create_output_folder(output: str) -> None:
+    if PurePath(output).is_absolute():
+        Path.mkdir(output, parents=True, exist_ok=True)
+    else:
+        Path.mkdir((Path().absolute().joinpath(output)), parents=True, exist_ok=True)
 
 if __name__ == "__main__":
     main()
