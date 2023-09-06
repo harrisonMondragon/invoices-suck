f52f2ca9dd4552d7a7666656985a6197aeeebf40
Harry <77893070+harrisonMondragon@users.noreply.github.com>
2023-09-05 23:36:52 -0600

    Add example files and minimal processing
    Add an example folder of sub files to work with. Also add code to open
    and print the contents of the sub files. Note that dependancies will
    need to be manually installed at this point.

--- invoices_suck.py
+++ invoices_suck.py
@@ -1,1 +1,16 @@
-print("The big brown fox jumped over the lazy dog")
+import sys
+import pandas as pd
+from pathlib import Path
+
+def main():
+
+    sub_folder = Path(sys.argv[1])
+    sub_files = sub_folder.glob('*')
+
+    for file in sub_files:
+        sub_df = pd.read_excel(file)
+        print(sub_df)
+
+
+if __name__ == "__main__":
+    main()
