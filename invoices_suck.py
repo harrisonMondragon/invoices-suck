60524f2fb29628264e635b26b5d94ebda7843ea0
Harry <77893070+harrisonMondragon@users.noreply.github.com>
2023-09-07 13:40:56 -0600

    Add function to create client directories
    Add a function to create  all client directories, can be called per sub
    DataFrame

--- invoices_suck.py
+++ invoices_suck.py
@@ -3,21 +3,30 @@ import pandas as pd
 from pathlib import Path, PurePath
 
 def main():
-
     sub_files = Path(sys.argv[1]).glob('*')
-    create_output_folder(sys.argv[2])
+    out_path = create_output_folder(sys.argv[2])
 
     for file in sub_files:
         sub_df = pd.read_excel(file)
         sub_df.sort_values(by='BILL_DATE')
+        make_directories(sub_df, out_path)
 
-        # print(sub_df)
-
-def create_output_folder(output: str) -> None:
+def create_output_folder(output: str) -> Path:
     if PurePath(output).is_absolute():
-        Path.mkdir(output, parents=True, exist_ok=True)
+        out = output
     else:
-        Path.mkdir((Path().absolute().joinpath(output)), parents=True, exist_ok=True)
+        out = Path().absolute().joinpath(output)
+
+    Path.mkdir(out, parents=True, exist_ok=True)
+    return out
+
+def make_directories(df: pd.DataFrame, out: Path) -> None:
+    for row in df.itertuples():
+        client_path = out.joinpath(getattr(row, "CLIENT_ID"))
+        if not client_path.is_dir():
+            Path.mkdir(client_path, parents=True, exist_ok=True)
+
+
 
 if __name__ == "__main__":
     main()
